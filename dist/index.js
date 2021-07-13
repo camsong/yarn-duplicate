f40a8fca3767e0fdcee1d34fd3c8220c76c0c578
会影 <neosoyn@gmail.com>
2021-07-13 14:42:56 +0800

    feat: add package size in browser

--- dist/index.js
+++ dist/index.js
@@ -7,43 +7,74 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const figlet_1 = __importDefault(require("figlet"));
 const yargs_1 = __importDefault(require("yargs"));
+const chalk_1 = __importDefault(require("chalk"));
 const lockfile_1 = require("@yarnpkg/lockfile");
-function consoleError(str) {
-    console.log(`\x1b[41m`, str);
-}
+const node_fetch_1 = __importDefault(require("node-fetch"));
 const args = yargs_1.default.options({
     'source': { type: 'string', demandOption: false, alias: 's', default: 'yarn.lock' }
 }).argv;
+// fetchFileSize('postcss-unique-selectors');
+async function fetchFileSize(name, version) {
+    if (name.startsWith('@ali') || name.startsWith('@types') || name.startsWith('@babel')) {
+        // @ali is interval packages, can not find size.
+        return Promise.resolve('-');
+    }
+    const url = version ? `https://bundlephobia.com/api/size?package=${name}@${version}` : `https://bundlephobia.com/api/size?package=${name}`;
+    return node_fetch_1.default(url).then(data => {
+        return data.json();
+    }).then(data => {
+        // console.log('data: ', data);
+        if (data === null || data === void 0 ? void 0 : data.size) {
+            // console.log('size in browser: ', (data.size / 1024).toFixed(2), 'kb');
+            return +(data.size / 1024).toFixed(2);
+        }
+        else if (data === null || data === void 0 ? void 0 : data.error) {
+            // console.error('Get package size error, url: ', url, '; code: ', data.error.code, '; message: ', data.error.message);
+            return '-';
+        }
+        else {
+            // unknow error
+            return '-';
+        }
+    }, e => {
+        // console.error('Get package size error, url: ', url, e);
+        return '-';
+    }).catch(e => {
+        // console.error('Get package size error, url: ', url, e);
+        return '-';
+    });
+}
 figlet_1.default.text('Yarn Duplicate', {
     horizontalLayout: 'default',
     verticalLayout: 'default',
     width: 100,
     whitespaceBreak: true
-}, function (err, data) {
+}, async function (err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
-    console.log(data);
+    console.log(chalk_1.default.gray(data));
     let filePath;
     if (args.source.startsWith('/')) {
         filePath = args.source;
         if (!fs_1.default.existsSync(filePath)) {
-            consoleError('File does not exists, ' + args.source);
+            console.error(chalk_1.default.red('File does not exists, ' + args.source));
             process.exit(0);
         }
     }
     else {
         filePath = process.cwd() + '/' + args.source;
+        // filePath = '/Users/cam/ali/bi-engine/yarn.lock'; // FIXME
         if (!fs_1.default.existsSync(filePath)) {
-            consoleError('No yarn.lock in current folder, please confirm, ' + process.cwd());
+            console.error(chalk_1.default.red('No yarn.lock in current folder, please confirm, ' + process.cwd()));
             process.exit(0);
         }
     }
-    getDuplicatedPkgs(filePath);
+    await getDuplicatedPkgs(filePath);
 });
-function getDuplicatedPkgs(path) {
+async function getDuplicatedPkgs(path) {
     const parsed = lockfile_1.parse(fs_1.default.readFileSync(path, 'utf8'));
     const installedPkgs = [];
     if (parsed && parsed.type === 'success') {
@@ -57,7 +88,7 @@ function getDuplicatedPkgs(path) {
      */
     let prevPkg = installedPkgs.length > 0 ? installedPkgs[0] : {};
     const duplicatedPkgTreeObj = {};
-    installedPkgs.forEach(pkg => {
+    installedPkgs.forEach(async (pkg) => {
         if (pkg.name === prevPkg.name && pkg.version !== prevPkg.version) {
             if (duplicatedPkgTreeObj[pkg.name] == null) {
                 duplicatedPkgTreeObj[pkg.name] = [prevPkg];
@@ -66,9 +97,24 @@ function getDuplicatedPkgs(path) {
         }
         prevPkg = pkg;
     });
-    console.log('The duplicate packages are as follows:');
-    const pkgs = Object.entries(duplicatedPkgTreeObj).map(([key, value]) => ({ name: key, value: value.length }));
-    pkgs.sort((a, b) => { return b.value - a.value; });
-    console.table(pkgs, ['name', 'value']);
+    if (Object.entries(duplicatedPkgTreeObj).length === 0) {
+        console.log(chalk_1.default.green('🎉 Awesome, you have no duplicate packages, that\'s amazing!'));
+    }
+    else {
+        console.log('😈 Summary: you have', chalk_1.default.red(Object.entries(duplicatedPkgTreeObj).length), 'duplicate packages in total, repeated', chalk_1.default.red(Object.entries(duplicatedPkgTreeObj).reduce((result, curr) => result + curr[1].length, 0)), 'times!');
+        console.log('👉 The details are as follows: ');
+        const P = ['\\', '|', '/', '-'];
+        let x = 0;
+        const loader = setInterval(() => {
+            process.stdout.write(chalk_1.default.gray(`\r${P[x++]}`));
+            x %= P.length;
+        }, 250);
+        const pkgs = await Promise.all(Object.entries(duplicatedPkgTreeObj).map(async ([key, value]) => ({
+            name: key, value: value.length, "sizeInBrowser-KB": await fetchFileSize(key)
+        })));
+        clearInterval(loader);
+        pkgs.sort((a, b) => { return b.value - a.value; });
+        console.table(pkgs, ['name', 'value', 'sizeInBrowser-KB']);
+    }
 }
 // TODO 基于重复的pkg，来做分析，找到重复的来源。
--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,78 @@
 import fs from 'fs';
 import figlet from 'figlet';
 import yargs from 'yargs';
+import chalk from 'chalk';
 import { parse } from '@yarnpkg/lockfile';
-
-function consoleError(str: string) {
-  console.log(`\x1b[41m`, str);
-}
+import fetch from 'node-fetch';
 
 const args = yargs.options({
   'source': { type: 'string', demandOption: false, alias: 's', default: 'yarn.lock' }
 }).argv;
 
+// fetchFileSize('postcss-unique-selectors');
+
+async function fetchFileSize(name: string, version?: string) {
+  if (name.startsWith('@ali') || name.startsWith('@types') || name.startsWith('@babel')) {
+    // @ali is interval packages, can not find size.
+    return Promise.resolve('-');
+  }
+
+  const url = version ? `https://bundlephobia.com/api/size?package=${name}@${version}` : `https://bundlephobia.com/api/size?package=${name}`;
+
+  return fetch(url).then(
+    data => {
+      return data.json();
+    }
+  ).then(data => {
+    // console.log('data: ', data);
+    if (data?.size) {
+      // console.log('size in browser: ', (data.size / 1024).toFixed(2), 'kb');
+      return +(data.size / 1024).toFixed(2);
+    } else if (data?.error) {
+      // console.error('Get package size error, url: ', url, '; code: ', data.error.code, '; message: ', data.error.message);
+      return '-';
+    } else {
+      // unknow error
+      return '-';
+    }
+  }, e => {
+    // console.error('Get package size error, url: ', url, e);
+    return '-';
+  }).catch(e => {
+    // console.error('Get package size error, url: ', url, e);
+    return '-';
+  });
+}
 
 figlet.text('Yarn Duplicate', {
   horizontalLayout: 'default',
   verticalLayout: 'default',
   width: 100,
   whitespaceBreak: true
-}, function(err, data) {
+}, async function (err, data) {
   if (err) {
-      console.log('Something went wrong...');
-      console.dir(err);
-      return;
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
   }
-  console.log(data);
+  console.log(chalk.gray(data));
 
   let filePath;
-  if((args as any).source.startsWith('/')) {
+  if ((args as any).source.startsWith('/')) {
     filePath = (args as any).source;
     if (!fs.existsSync(filePath)) {
-      consoleError('File does not exists, ' + (args as any).source);
+      console.error(chalk.red('File does not exists, ' + (args as any).source));
       process.exit(0);
     }
   } else {
     filePath = process.cwd() + '/' + (args as any).source;
+    // filePath = '/Users/cam/ali/bi-engine/yarn.lock'; // FIXME
     if (!fs.existsSync(filePath)) {
-      consoleError('No yarn.lock in current folder, please confirm, ' + process.cwd());
+      console.error(chalk.red('No yarn.lock in current folder, please confirm, ' + process.cwd()));
       process.exit(0);
     }
   }
-  getDuplicatedPkgs(filePath);
+  await getDuplicatedPkgs(filePath);
 });
 
 interface PkgItem {
@@ -52,11 +85,11 @@ interface PkgItem {
   dependencies: { [key: string]: string }[];
 }
 
-function getDuplicatedPkgs(path: string) {
+async function getDuplicatedPkgs(path: string) {
   const parsed = parse(fs.readFileSync(path, 'utf8'));
 
   const installedPkgs = [] as PkgItem[];
-  
+
   if (parsed && parsed.type === 'success') {
     Object.entries(parsed.object).forEach(([key, value]) => {
       installedPkgs.push({
@@ -66,15 +99,15 @@ function getDuplicatedPkgs(path: string) {
       });
     });
   }
-  
+
   /**
    * 存放重复的包。
    * 计算逻辑：installedPkgs 是安装字母排序的，匹配 name 和 installedVersion 如果 name 与上一个相同，但 installedVersion 不同就说明包重复了
    */
   let prevPkg = installedPkgs.length > 0 ? installedPkgs[0] : {} as PkgItem;
-  
-  const duplicatedPkgTreeObj: {[key: string]: PkgItem[]} = {};
-  installedPkgs.forEach(pkg => {
+
+  const duplicatedPkgTreeObj: { [key: string]: PkgItem[] } = {};
+  installedPkgs.forEach(async pkg => {
     if (pkg.name === prevPkg.name && pkg.version !== prevPkg.version) {
       if (duplicatedPkgTreeObj[pkg.name] == null) {
         duplicatedPkgTreeObj[pkg.name] = [prevPkg];
@@ -84,10 +117,29 @@ function getDuplicatedPkgs(path: string) {
     prevPkg = pkg;
   });
 
-  console.log('The duplicate packages are as follows:');
-  const pkgs = Object.entries(duplicatedPkgTreeObj).map(([key, value]) => ({ name: key, value: value.length}));
-  pkgs.sort((a, b) =>  { return b.value - a.value; });
-  console.table(pkgs, ['name', 'value']);
+  if (Object.entries(duplicatedPkgTreeObj).length === 0) {
+    console.log(chalk.green('🎉 Awesome, you have no duplicate packages, that\'s amazing!'));
+  } else {
+    console.log('😈 Summary: you have', chalk.red(Object.entries(duplicatedPkgTreeObj).length), 'duplicate packages in total, repeated', 
+    chalk.red(Object.entries(duplicatedPkgTreeObj).reduce((result, curr) => result + curr[1].length, 0)), 'times!');
+    console.log('👉 The details are as follows: ');
+    
+    const P = ['\\', '|', '/', '-'];
+    let x = 0;
+    const loader = setInterval(() => {
+      process.stdout.write(chalk.gray(`\r${P[x++]}`));
+      x %= P.length;
+    }, 250);
+
+    const pkgs = await Promise.all(Object.entries(duplicatedPkgTreeObj).map(async ([key, value]): Promise<any> => ({
+      name: key, value: value.length, "sizeInBrowser-KB": await fetchFileSize(key)
+    })));
+
+    clearInterval(loader);
+
+    pkgs.sort((a, b) => { return b.value - a.value; });
+    console.table(pkgs, ['name', 'value', 'sizeInBrowser-KB']);
+  }
 }
 
 // TODO 基于重复的pkg，来做分析，找到重复的来源。
